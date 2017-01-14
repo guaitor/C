814df6c1a5b7e3324ba49a54bc964b6006b62c2d
goguaitor <aitor.gonzalez@gmail.com>
2017-01-14 18:32:25 +0100

    Create suma.c

---
+++ suma.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	float a,b,c;
+	printf("\nPrimer sumando:");
+	scanf("%f",&a);
+	prinf("\nSegundo sumando:");
+	scanf("%f",&b);
+	c=a+b;
+	prinf("\n\nLa suma de %f y %f es %f", a,b,c);
+	
+}
